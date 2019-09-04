a0643d775765bc8beed6b733fab7331d20414c1e
Moneera Banjar <mn.banjar@gmail.com>
2019-09-04 16:02:28 +0300

    1sr version

--- downloadupdateclosure.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ ! -d "closure" ]; then
-    mkdir closure
-fi
-
-wget http://closure-compiler.googlecode.com/files/compiler-latest.zip
-yes | unzip -d closure compiler-latest.zip > /dev/null 2>&1
-rm compiler-latest.zip
-
